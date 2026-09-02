76c472fc84c107bebdad15c8c571844578443333
Neelesh Tiwari <225562583+neelesh500@users.noreply.github.com>
2026-09-02 06:29:50 +0530

    Time: 0 ms (100%), Space: 30.2 MB (67.8%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
+        int odd_count = 0;
+        for (int num : nums1) {
+            if (num % 2 != 0) {
+                odd_count++;
+            }
+        }
         return true;
     }
 };
